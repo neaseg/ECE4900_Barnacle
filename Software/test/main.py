e4d53c5aac662cadcd48dfc3c0dc2ff1f5369069
Benjamin Papineau <benjaminpap@hotmail.com>
2018-04-08 12:48:51 -0400

    don't look too close

--- Software/test/main.py
+++ Software/test/main.py
@@ -119,8 +119,7 @@ Builder.load_string("""
 			font_size: 75
 			size_hint: 1,0.8
 			text: "DIAL 911"
-			on_press:  self.send_text_message()
-			on_release: self.activate()
+			on_release:  self.send_text_message()
 			pos_hint: {"right":1, "top":1}
 
 
@@ -332,10 +331,72 @@ Builder.load_string("""
 
 """)
 class HomePage(Screen):
-	pass
-	# if(trigger):
-	# 	IntentButton.send_text_message()
+	def discover_device():
+	    # Clear any cached data because both bluez and CoreBluetooth have issues with
+	    # caching data and it going stale.
+		ble.clear_cached_data()
+	    # Get the first available BLE network adapter and make sure it's powered on.
+		adapter = ble.get_default_adapter()
+		adapter.power_on()
+		print('Using adapter: {0}'.format(adapter.name))
+		# Disconnect any currently connected UART devices.  Good for cleaning up and
+		# starting from a fresh state.
+		print('Disconnecting any connected UART devices...')
+		UART.disconnect_devices()
+		# Scan for UART devices.
+		print('Searching for UART device...')
+		try:
+			adapter.start_scan()
+			# Search for the first UART device found (will time out after 60 seconds
+			# but you can specify an optional timeout_sec parameter to change it).
+			device = UART.find_device()
+			if device is None:
+				raise RuntimeError('Failed to find UART device!')
+		finally:
+	        # Make sure scanning is stopped before exiting.
+			adapter.stop_scan()
 
+			print('Connecting to device...')
+			device.connect()# Will time out after 60 seconds, specify timeout_sec parameter
+	                      # to change the timeout.
+	    # Once connected do everything else in a try/finally to make sure the device
+	    # is disconnected when done.
+	        # Wait for service discovery to complete for the UART service.  Will
+	        # time out after 60 seconds (specify timeout_sec parameter to override).
+			print('Discovering services...')
+			UART.discover(device)
+	        # Once service discovery is complete create an instance of the service
+	        # and start interacting with it.
+			uart = UART(device)
+			return uart
+
+	def message_device(uart):
+	    # Write a string to the TX characteristic.
+	    uart.write('Hello world!\r\n')
+	    print("Sent 'Hello world!' to the device.")
+
+
+	        # Now wait up to one minute to receive data from the device.
+	def recieve_message(uart):
+		ans = False
+		received = uart.read(timeout_sec=5)
+		if received is not None:
+			# Received data, print it out.
+			print('Received: {0}'.format(received))
+			ans = True
+			return ans
+
+	def run_toggle(self,*args):
+		if (Main.startup):
+			Main.startup = False;
+		else:
+			if Main.uart is None:
+				Main.uart = discover_device()
+			else:
+				if(recieve_message(Main.uart)):
+					IntentButton.send_text_message()
+
+	run_toggle
 
 class Contact(Screen):
 	pass
@@ -398,85 +459,12 @@ class IntentButton(Button):
 				if i:
 					backend.send_sms(i,Main.message)
 
-	def activate(self, *args):
-		ble.run_mainloop_with(why_are_you_looking_this_close(Main.trigger))
-		if Main.trigger:
-			send_text_message
-
-
-
 
 # Main function implements the program logic so it can run in a background
 # thread.  Most platforms require the main thread to handle GUI events and other
 # asyncronous events like BLE actions.  All of the threading logic is taken care
 # of automatically though and you just need to provide a main function that uses
 # the BLE provider.
-def why_are_you_looking_this_close(trigger):
-    # Clear any cached data because both bluez and CoreBluetooth have issues with
-    # caching data and it going stale.
-    ble.clear_cached_data()
-
-    # Get the first available BLE network adapter and make sure it's powered on.
-    adapter = ble.get_default_adapter()
-    adapter.power_on()
-    print('Using adapter: {0}'.format(adapter.name))
-
-    # Disconnect any currently connected UART devices.  Good for cleaning up and
-    # starting from a fresh state.
-    print('Disconnecting any connected UART devices...')
-    UART.disconnect_devices()
-
-    # Scan for UART devices.
-    print('Searching for UART device...')
-    try:
-        adapter.start_scan()
-        # Search for the first UART device found (will time out after 60 seconds
-        # but you can specify an optional timeout_sec parameter to change it).
-        device = UART.find_device()
-        if device is None:
-            raise RuntimeError('Failed to find UART device!')
-    finally:
-        # Make sure scanning is stopped before exiting.
-        adapter.stop_scan()
-
-    print('Connecting to device...')
-    device.connect()  # Will time out after 60 seconds, specify timeout_sec parameter
-                      # to change the timeout.
-
-    # Once connected do everything else in a try/finally to make sure the device
-    # is disconnected when done.
-
-    beta = True;
-    while beta:
-        try:
-        # Wait for service discovery to complete for the UART service.  Will
-        # time out after 60 seconds (specify timeout_sec parameter to override).
-            print('Discovering services...')
-            UART.discover(device)
-        # Once service discovery is complete create an instance of the service
-        # and start interacting with it.
-            uart = UART(device)
-            beta = False;
-        finally:
-            print('Didnt connect initally. Trying again')
-
-
-    try:
-		# Now wait up to 20 seconds to receive data from the device.
-        print('Waiting up to 20 seconds to receive data from the device...')
-        received = uart.read(timeout_sec=20)
-        if received is not None:
-            # Received data, print it out.
-            print('Received: {0}'.format(received))
-            # Write a string to the TX characteristic.
-            print("Sent 'Hello world!' to the device.")
-            uart.write('Hello world!\r\n')
-            trigger = True;
-        else:
-            # Timeout waiting for data, None is returned.
-            print('Received no data!')
-    finally:
-    	device.disconnect()
 
 
 screen_manager = ScreenManager()
@@ -493,7 +481,8 @@ class Main(App):
 	contacts = ['','','',''] # can select contacts by Main.contacts
 	message = '' #can access messsage by using Main.message
 	user_info = ['',''] #access user information by using Main.
-	trigger = False;
+	startup = True
+	uart = None;
 	def build(self):
 		return screen_manager
 
