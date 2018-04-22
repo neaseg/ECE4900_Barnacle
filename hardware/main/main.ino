818e579747796401110127b65065c7cb83f661dc
Garrett Nease <garrett.nease@gmail.com>
2018-04-22 10:27:57 -0400

    Update main.ino

--- hardware/main/main.ino
+++ hardware/main/main.ino
@@ -9,60 +9,24 @@
 #if SOFTWARE_SERIAL_AVAILABLE
   #include <SoftwareSerial.h>
 #endif
+//pins 5 and 6 are free
+int inPin = 6;   // input pin for pushbutton
+int pinStat = 0; // variable for reading pin status
+int led = 5;     // receive data led
+int led_send = 3;// send data led
+//const int ledP = 13;      // led connected to digital pin 13, not used at the moment
+const int knockSensor = A0; // the piezo is connected to analog pin 0
+const int threshold = 500;  // threshold value to decide when the detected sound is a knock or not
+int sensorReading = 0;      // variable to store the value read from the sensor pin
+int ledState = LOW;         // variable used to store the last LED status, to toggle the light
 
-/*=========================================================================
-    APPLICATION SETTINGS
-    FACTORYRESET_ENABLE       Perform a factory reset when running this sketch
-   
-                              Enabling this will put your Bluefruit LE module
-                              in a 'known good' state and clear any config
-                              data set in previous sketches or projects, so
-                              running this at least once is a good idea.
-   
-                              When deploying your project, however, you will
-                              want to disable factory reset by setting this
-                              value to 0.  If you are making changes to your
-                              Bluefruit LE device via AT commands, and those
-                              changes aren't persisting across resets, this
-                              is the reason why.  Factory reset will erase
-                              the non-volatile memory where config data is
-                              stored, setting it back to factory default
-                              values.
-       
-                              Some sketches that require you to bond to a
-                              central device (HID mouse, keyboard, etc.)
-                              won't work at all with this feature enabled
-                              since the factory reset will clear all of the
-                              bonding data stored on the chip, meaning the
-                              central device won't be able to reconnect.
-    MINIMUM_FIRMWARE_VERSION  Minimum firmware version to have some new features
-    MODE_LED_BEHAVIOUR        LED activity, valid options are
-                              "DISABLE" or "MODE" or "BLEUART" or
-                              "HWUART"  or "SPI"  or "MANUAL"
-    -----------------------------------------------------------------------*/
     #define FACTORYRESET_ENABLE         1
     #define MINIMUM_FIRMWARE_VERSION    "0.6.6"
     #define MODE_LED_BEHAVIOUR          "MODE"
 /*=========================================================================*/
 
-// Create the bluefruit object, either software serial...uncomment these lines
-/*
-SoftwareSerial bluefruitSS = SoftwareSerial(BLUEFRUIT_SWUART_TXD_PIN, BLUEFRUIT_SWUART_RXD_PIN);
-Adafruit_BluefruitLE_UART ble(bluefruitSS, BLUEFRUIT_UART_MODE_PIN,
-                      BLUEFRUIT_UART_CTS_PIN, BLUEFRUIT_UART_RTS_PIN);
-*/
-
-/* ...or hardware serial, which does not need the RTS/CTS pins. Uncomment this line */
-// Adafruit_BluefruitLE_UART ble(Serial1, BLUEFRUIT_UART_MODE_PIN);
-
 /* ...hardware SPI, using SCK/MOSI/MISO hardware SPI pins and then user selected CS/IRQ/RST */
 Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_CS, BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
-
-/* ...software SPI, using SCK/MOSI/MISO user-defined SPI pins and then user selected CS/IRQ/RST */
-//Adafruit_BluefruitLE_SPI ble(BLUEFRUIT_SPI_SCK, BLUEFRUIT_SPI_MISO,
-//                             BLUEFRUIT_SPI_MOSI, BLUEFRUIT_SPI_CS,
-//                             BLUEFRUIT_SPI_IRQ, BLUEFRUIT_SPI_RST);
-
 
 // A small helper
 void error(const __FlashStringHelper*err) {
@@ -76,22 +40,16 @@ void error(const __FlashStringHelper*err) {
             automatically on startup)
 */
 /**************************************************************************/
-//initialize pins for the push button
-
-int inPin = 7;   //input pin for pushbutton
-int pinStat = 0; //variable for reading pin status
-int i = 0;       //loop variable 
-
 void setup(void)
 {
- {
+  pinMode(led_send, OUTPUT);
+  pinMode(led, OUTPUT);
   pinMode(inPin, INPUT); //declare pushbutton as input
- }
   //while (!Serial);  // required for Flora & Micro
   delay(500);
 
   Serial.begin(115200);
-  Serial.println(F("Button Data Send Test"));
+  Serial.println(F("ECE 4900 Demo"));
   Serial.println(F("---------------------------------------"));
 
   /* Initialise the module */
@@ -119,9 +77,9 @@ void setup(void)
   /* Print Bluefruit information */
   ble.info();
 
-  /* Serial.println(F("Please use Adafruit Bluefruit LE app to connect in UART mode"));
+  Serial.println(F("Please use Adafruit Bluefruit LE app to connect in UART mode"));
   Serial.println(F("Then Enter characters to send to Bluefruit"));
-  Serial.println(); */
+  Serial.println();
 
   ble.verbose(false);  // debug info is a little annoying after this point!
 
@@ -139,27 +97,98 @@ void setup(void)
     ble.sendCommandCheckOK("AT+HWModeLED=" MODE_LED_BEHAVIOUR);
     Serial.println(F("******************************"));
   }
-
 }
 
 /**************************************************************************/
 /*!
-    @brief  Constantly poll for button press
+    @brief  Constantly poll for new command or response data
 */
 /**************************************************************************/
 void loop(void)
 {
- pinStat = digitalRead(inPin); //read input pin
- if (pinStat == HIGH){    //if the button is pressed
+  // Check for user input
+  char inputs[BUFSIZE+1];
+*/
+ // read the sensor and store it in the variable sensorReading:
+  sensorReading = analogRead(knockSensor);
+  if (sensorReading >= threshold) {
+    ble.print("AT+BLEUARTTX=");
+    ble.println(9);
+    
+    // check response stastus
+    if ( ble.waitForOK() ) {
+      digitalWrite(led_send, HIGH);
+      delay(500);
+      digitalWrite(led_send, LOW);
+    }
+    else {
+      Serial.println(F("Failed to send?"));
+    }
+  }
+  
+ //send ascii 8 to the connected device
+  pinStat = digitalRead(inPin); //read input pin
+  if (pinStat == HIGH){    //if the button is pressed
   //for(i=0;i<5;i++){
-    delay(500);
     ble.print("AT+BLEUARTTX=");
     ble.println(8);
     
     // check response stastus
-    if (! ble.waitForOK() ) {
+    if ( ble.waitForOK() ) {
+      digitalWrite(led_send, HIGH);
+      delay(500);
+      digitalWrite(led_send, LOW);
+    }
+    else {
       Serial.println(F("Failed to send?"));
     }
   pinStat = 0;
+  //delay(500);//delay to allow person to take their finger off the button
  }
+
+ 
+  // Check for incoming characters from Bluefruit
+  ble.println("AT+BLEUARTRX");
+  ble.readline();
+  if (strcmp(ble.buffer, "OK") == 0) {
+    // no data
+    return;
+  }
+  
+  // Some data was found, its in the buffer
+  Serial.print(F("[Recv] ")); Serial.println(ble.buffer);
+  
+  if ( ble.waitForOK() ){
+    digitalWrite(led, HIGH);
+    delay(1000);
+    digitalWrite(led, LOW);
+  }
+  //pinStat = 0;
+  
+}
+
+/**************************************************************************/
+/*!
+    @brief  Checks for user input (via the Serial Monitor)
+*/
+/**************************************************************************/
+bool getUserInput(char buffer[], uint8_t maxSize)
+{
+  // timeout in 100 milliseconds
+  TimeoutTimer timeout(100);
+
+  memset(buffer, 0, maxSize);
+  while( (!Serial.available()) && !timeout.expired() ) { delay(1); }
+
+  if ( timeout.expired() ) return false;
+
+  delay(2);
+  uint8_t count=0;
+  do
+  {
+    count += Serial.readBytes(buffer+count, maxSize);
+    delay(2);
+  } while( (count < maxSize) && (Serial.available()) );
+
+  return true;
 }
